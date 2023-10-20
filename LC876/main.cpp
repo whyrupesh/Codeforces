cf579d5bcec413a4d32ec0aa4d126aa07e58a5a8
whyrupesh <srupeshkumar800@gmail.com>
2023-10-20 22:05:03 +0530

    A good question to understand loop for linked list

---
+++ LC876/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast && fast->next){  //continues as long as both pointers are not NULL
+            //if any one is null it stops executing
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+};
